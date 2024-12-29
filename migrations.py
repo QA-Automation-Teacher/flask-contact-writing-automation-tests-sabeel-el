85c89e60d9e9506920213bc8b2be70a1130b03f9
sabeel-el <sabeel@post.bgu.ac.il>
2024-12-29 13:32:50 +0200

    updated migrations.py

--- migrations.py
+++ migrations.py
@@ -1,20 +1,23 @@
 from models import db, Contacts
 from faker import Factory
 
+from app import app 
+
 fake = Factory.create()
 # Spanish
 #fake = Factory.create('es_ES')
+with app.app_context():
 # Reload tables
-db.drop_all()
-db.create_all()
-# Make 100 fake contacts
-for num in range(100):
-    fullname = fake.name().split()
-    name = fullname[0]
-    surname = ' '.join(fullname[1:])
-    email = fake.email()
-    phone = fake.phone_number()
-    # Save in database
-    mi_contacto = Contacts(name, surname, email, phone)
-    db.session.add(mi_contacto)
-    db.session.commit()
+    db.drop_all()
+    db.create_all()
+    # Make 100 fake contacts
+    for num in range(100):
+        fullname = fake.name().split()
+        name = fullname[0]
+        surname = ' '.join(fullname[1:])
+        email = fake.email()
+        phone = fake.phone_number()
+        # Save in database
+        mi_contacto = Contacts(name, surname, email, phone)
+        db.session.add(mi_contacto)
+        db.session.commit()
